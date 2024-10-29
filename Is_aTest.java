5e2849dce206aa0c88d0274009b9cc884963394a
Raghad Mohamed <raghaduni1004@gmail.com>
2024-10-30 02:24:12 +0300

    Add files via upload

---
+++ Is_aTest.java
@@ -0,0 +1,45 @@
+package test;
+// src/MainTest.java
+import static org.junit.Assert.*;
+
+
+import org.junit.Test; // Ensure this is correct
+
+import main.CLI;
+
+// java -cp "libs/*;bin" org.junit.runner.JUnitCore MainTest
+public class Is_aTest {
+
+
+    // Testing when the command have been enterd correctly 
+    @Test 
+    public void Is_aCorrect() {
+        CLI cli = new CLI();
+        String expected = "Files and directories in the current directory:" + System.lineSeparator() +
+                          ".vscode" + System.lineSeparator() +
+                          "bin" + System.lineSeparator() +
+                          "libs" + System.lineSeparator() +
+                          "src" + System.lineSeparator();
+                      
+        @SuppressWarnings("static-access")
+        String result = cli.Is_a("Is-a");
+        assertEquals(expected, result);
+    }
+
+
+    // Testing if the command has been mis spelled 
+    @Test 
+    public void Is_aMissSpelled(){
+        String expected = "Did you mean Is-a?";
+                      
+        String result = CLI.Is_a("I-");
+        assertEquals(expected, result);
+
+        String result1 = CLI.Is_a("is-");
+        assertEquals(expected, result1);
+
+        String result2 = CLI.Is_a("Is a");
+        assertEquals(expected, result2);
+    }
+      
+}
